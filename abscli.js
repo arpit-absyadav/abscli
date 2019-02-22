fed42e05e0d68d3db83ecc8ef7c901839d1dd0be
Arpit <arpit.absyadav@hotmail.com>
2019-02-23 01:58:23 +0530

    server created init

--- abscli.js
+++ abscli.js
@@ -4,7 +4,7 @@
  * @Author: Arpit.Yadav
  * @Date: 2019-02-20 21:15:26
  * @Last Modified by: Arpit.Yadav
- * @Last Modified time: 2019-02-22 14:51:26
+ * @Last Modified time: 2019-02-23 00:59:11
  */
 
 // const inquirer = require('inquirer');
@@ -13,7 +13,8 @@ const figlet = require('figlet');
 // const program = require('commander');
 const shell = require('shelljs');
 const _ = require('lodash');
-
+const { generateModule } = require('./moduleGenerator');
+const { generateServer } = require('./serverGenerator');
 const init = () => {
   console.log(
     chalk.green(
@@ -25,77 +26,27 @@ const init = () => {
     ),
   );
 };
+const help = async () => {
+  console.log(chalk.red('WRONG INPUT: ') + ` Please pass valid command`);
+  console.log(chalk.white('TO CREATE SERVER: ') + chalk.yellow('abscli --server  servername'));
+  console.log(chalk.white('TO CREATE MODULE: ') + chalk.yellow('abscli --module  modulename'));
+};
 const run = async () => {
   init();
   // eslint-disable-next-line no-undef
-  let moduleName = process.argv.slice(2);
-  console.log(moduleName);
-
-  await shell.exec(`git clone --depth=1  https://github.com/arpit-absyadav/____server-module.git ${moduleName}`);
-  await shell.exec(` rm -rf .git`);
-  // remaming Files
-
-  // Model Creating
-  console.log(chalk.green('CREATING ') + `[Schema]  ./${moduleName}/${moduleName}.model.js`);
-  await shell.exec(`mv ./${moduleName}/school.model.js ./${moduleName}/${moduleName}.model.js`);
-
-  // Routes creating
-  console.log(chalk.green('CREATING ') + `[Route]  ./${moduleName}/${moduleName}.routes.js`);
-  await shell.exec(`mv ./${moduleName}/school.routes.js ./${moduleName}/${moduleName}.routes.js`);
-
-  // Controller creating
-  console.log(chalk.green('CREATING ') + `[Controller]  ./${moduleName}/${moduleName}.controller.js`);
-  await shell.exec(`mv ./${moduleName}/school.controller.js ./${moduleName}/${moduleName}.controller.js`);
-
-  // Service creating
-  console.log(chalk.green('CREATING ') + `[Service]  ./${moduleName}/${moduleName}.service.js`);
-  await shell.exec(`mv ./${moduleName}/school.service.js ./${moduleName}/${moduleName}.service.js`);
-
-  // Validator creating
-  console.log(chalk.green('CREATING ') + `[Validator]  ./${moduleName}/${moduleName}.validator.js`);
-  await shell.exec(`mv ./${moduleName}/school.validator.js ./${moduleName}/${moduleName}.validator.js`);
-  console.log(chalk.green('CREATING ') + `[Validator]  ./${moduleName}/validators/registration.validator.js`);
-  console.log(chalk.green('CREATING ') + `[Validator]  ./${moduleName}/validators/login.validator.js`);
-
-  // Changing Some texts in files
-  // Model
-  let moduleStartcase = _.startCase(moduleName);
-  await shell.exec(`sed -i 's/SchoolSchema/${moduleName}Schema/g' ./${moduleName}/${moduleName}.model.js `);
-  await shell.exec(`sed -i 's/School/${moduleStartcase}/g' ./${moduleName}/${moduleName}.model.js `);
-  //   // Routes
-  await shell.exec(`sed -i 's/school/${moduleName}/g' ./${moduleName}/${moduleName}.routes.js `);
-  await shell.exec(`sed -i 's/School/${moduleStartcase}/g' ./${moduleName}/${moduleName}.routes.js `);
-  await shell.exec(`sed -i 's/_schoolValidator/_${moduleName}Validator/g' ./${moduleName}/${moduleName}.routes.js `);
-
-  // Service
-  await shell.exec(`sed -i 's/School/${moduleStartcase}/g' ./${moduleName}/${moduleName}.service.js `);
-  await shell.exec(`sed -i 's/school/${moduleName}/g' ./${moduleName}/${moduleName}.service.js `);
-
-  // Controller
-  await shell.exec(`sed -i 's/School/${moduleStartcase}/g' ./${moduleName}/${moduleName}.controller.js `);
-  await shell.exec(`sed -i 's/school/${moduleName}/g' ./${moduleName}/${moduleName}.controller.js `);
-
-  // Validator
-  await shell.exec(`sed -i 's/school/${moduleName}/g' ./${moduleName}/${moduleName}.validator.js `);
-  await shell.exec(`sed -i 's/School/${moduleStartcase}/g' ./${moduleName}/${moduleName}.validator.js `);
-  // Validator folder
-  await shell.exec(`sed -i 's/school/${moduleName}/g' ./${moduleName}/validators/registration.validator.js `);
-  await shell.exec(`sed -i 's/school/${moduleName}/g' ./${moduleName}/validators/other.validator.js 
-  `);
-  await shell.exec(`sed -i 's/School/${moduleStartcase}/g' ./${moduleName}/validators/other.validator.js 
-  `);
-
-  // upateing Express.js
-  console.log(chalk.yellow('UPDATING ') + `[Express Config]  ../../config/express.js`);
-  await shell.exec(
-    `sed -i '/aboveroutes/i require("./../app/modules/${moduleName}/${moduleName}.routes")(app)'  ../../config/express.js  `,
-  );
-
-  // upateing Mongoose.js
-  console.log(chalk.yellow('UPDATING ') + `[Mongoose Config]  ../../config/database/mongoose.js`);
-  await shell.exec(
-    `sed -i '/schemas/i require("./../../app/modules/${moduleName}/${moduleName}.model")'  ../../config/database/mongoose.js`,
-  );
+  let passedArgs = process.argv.slice(2);
+  console.log(passedArgs);
+  if (passedArgs.length == 2) {
+    if (passedArgs[0] == '--server') {
+      generateServer(passedArgs[1]);
+    } else if (passedArgs[0] == '--module') {
+      generateModule(passedArgs[1]);
+    } else {
+      help();
+    }
+  } else {
+    help();
+  }
 };
 
 run();
---
+++ serverGenerator.js
@@ -0,0 +1,28 @@
+const chalk = require('chalk');
+const shell = require('shelljs');
+const _ = require('lodash');
+
+exports.generateServer = async function(serverName) {
+  console.log('Welcome to server Generator');
+  await shell.exec(`git clone --depth=1  https://github.com/arpit-absyadav/flex-server-mongodb.git ${serverName}`);
+  await shell.exec(` rm -rf .git`);
+  await shell.exec(` rm -rf ${serverName}/.git`);
+
+  console.log(chalk.green('CREATING ') + `[NPM] ` + chalk.blue('creating package.json '));
+  console.log(chalk.green('CREATING ') + `[SERVER] ` + chalk.blue('creating server.js '));
+  console.log(chalk.green('CREATING ') + `[EXPRESS] ` + chalk.blue('creating express.js '));
+  console.log(chalk.green('CREATING ') + `[DOCKER] ` + chalk.blue('creating docker files.'));
+  console.log(chalk.green('CREATING ') + `[JEST] ` + chalk.blue('creating jest configuration. '));
+  console.log(chalk.green('CREATING ') + `[CONFIG] ` + chalk.blue('creating configuration (env).'));
+  console.log(chalk.green('CREATING ') + `[MONGOOSE] ` + chalk.blue('creating configuration (database).'));
+  console.log(chalk.green('CREATING ') + `[HANDLERS] ` + chalk.blue('creating handlers. '));
+  console.log(chalk.green('CREATING ') + `[HELPERS] ` + chalk.blue('creating helpers. '));
+  console.log(chalk.green('CREATING ') + `[PROVIDERS] ` + chalk.blue('creating providers. '));
+  console.log(chalk.green('CREATING ') + `[MAILER] ` + chalk.blue('creating mail sender. '));
+
+  console.log(chalk.green('CREATING ') + `[MODULE] ` + chalk.blue('creating user module. '));
+  console.log(chalk.yellow('WAIT ') + `[NPM INSTALLING PACKAGES ....] `);
+  shell.exec(`cd ${serverName} && npm i `);
+  console.log(chalk.green('CONGRATULATIONS: ') + ` Your Server is ready. `);
+};
+// module.exports = generateServer;
